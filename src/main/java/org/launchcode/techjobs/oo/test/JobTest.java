bc8b42e9c36c8f7eb7fac43a002437a5d9409d95
Uzo <uzoikejiofor@gmail.com>
2022-10-03 21:17:31 -0400

    JobTest

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,55 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+
+    @Test
+    public void testSettingJobId () {
+        Job jobOne = new Job();
+        Job jobTwo = new Job();
+
+        assertNotEquals(jobOne, jobTwo);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+       Job testJob = new Job("Product tester", new Employer("Acme"), new Location("Desert")
+                ,new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertTrue(testJob.getName() instanceof String);
+        assertEquals(testJob.getName(), "Product tester");
+
+        assertTrue(testJob.getEmployer() instanceof Employer);
+        assertEquals(testJob.getEmployer().getValue(), "Acme");
+
+        assertTrue(testJob.getLocation() instanceof Location);
+        assertEquals(testJob.getLocation().getValue(), "Desert");
+
+        assertTrue(testJob.getPositionType() instanceof PositionType);
+        assertEquals(testJob.getPositionType().getValue(), "Quality control");
+
+        assertTrue(testJob.getCoreCompetency() instanceof CoreCompetency);
+        assertEquals(testJob.getCoreCompetency().getValue(), "Persistence");
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job equalityJobOne = new Job();
+        Job equalityJobTwo = new Job();
+
+        assertFalse(equalityJobOne.equals(equalityJobTwo));
+    }
+
+
 
 }
